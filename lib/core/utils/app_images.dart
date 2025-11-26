7bfe1d96a65ecea364d6d0e011fcfb8f0a78dd02
abdallahserag <abdoserag862@gmail.com>
2025-11-26 18:15:35 +0200

    appImages

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -1,3 +1,11 @@
 class AppImages{
  static const String splash = 'assets/images/splash.png';
+ static const String iconCart = 'assets/images/iconcart.png';
+ static const String iconFavorite = 'assets/images/iconfavorite.png';
+ static const String iconHome = 'assets/images/iconhome.png';
+ static const String iconUser = 'assets/images/iconuser.png';
+ static const String iconProduct = 'assets/images/iconproduct.png';
+ static const String imageSlide1 = 'assets/images/imageslide1.png';
+ static const String imageSlide2 = 'assets/images/imageslide2.png';
+ static const String imageSlide3 = 'assets/images/imageslide3.png';
 }
